7a84d8a59f1bb8b860d5900de1e6a04a069555a1
JPNAKAYAMA <jpn_rufino@hotmail.com>
2023-03-02 09:20:51 -0300

    teste 1 concluído - algoritmo soma

---
+++ 1-algoritmo-soma.js
@@ -0,0 +1,24 @@
+/*1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+enquanto K < INDICE faça
+{
+K = K + 1;
+SOMA = SOMA + K;
+}
+imprimir(SOMA);
+
+Ao final do processamento, qual será o valor da variável SOMA?*/
+
+let soma = 0
+let k = 0
+let i = 13
+
+while (k < i) {
+  k = k + 1
+  soma = soma + k
+}
+
+console.log(soma)
+
+// REPOSTA: Ao final do processo o valor de SOMA será 91.
